73396e1a906bbd06a4e923b0e72d454fb2309af1
Ngocnkn <ngockhac01012000@gmail.com>
2019-11-18 10:43:19 +0700

    dle

--- Ap_music/app/src/main/java/com/example/ap_music/fragments/artist/ArtistItemListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.ap_music.fragments.artist;
-
-import com.example.ap_music.base.BaseAdapter;
-import com.example.ap_music.model.Artist;
-
-public interface ArtistItemListener extends BaseAdapter.BaseItemListener {
-    void onArtistClick(Artist artist);
-}
