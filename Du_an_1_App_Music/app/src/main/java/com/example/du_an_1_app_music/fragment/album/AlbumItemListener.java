6b159991f025bad361ba8c06fb0becf5984ae684
Ngocnkn <ngockhac01012000@gmail.com>
2019-11-25 10:46:23 +0700

    dlt

--- Du_an_1_App_Music/app/src/main/java/com/example/du_an_1_app_music/fragment/album/AlbumItemListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.du_an_1_app_music.fragment.album;
-
-import com.example.du_an_1_app_music.base.BaseAdapter;
-import com.example.du_an_1_app_music.model.Album;
-
-public interface AlbumItemListener extends BaseAdapter.BaseItemListener {
-    void onAlbumItemClick(Album album);
-}
