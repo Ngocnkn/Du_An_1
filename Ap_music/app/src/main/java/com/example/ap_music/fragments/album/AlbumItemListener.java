0e92b16eaca7d6780eca4ccbc64ae9876dadab84
Ngocnkn <ngockhac01012000@gmail.com>
2019-12-08 14:26:22 +0700

    dlt

--- Ap_music/app/src/main/java/com/example/ap_music/fragments/album/AlbumItemListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.ap_music.fragments.album;
-
-import com.example.ap_music.base.BaseAdapter;
-import com.example.ap_music.model.Album;
-
-public interface AlbumItemListener extends BaseAdapter.BaseItemListener {
-    void albumItemClick(Album album);
-}
--- Ap_music/app/src/main/java/com/example/ap_music/views/PlayViewListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.ap_music.views;
-
-public interface PlayViewListener {
-    void onNext();
-    void onPrev();
-    void onPlay();
-}
