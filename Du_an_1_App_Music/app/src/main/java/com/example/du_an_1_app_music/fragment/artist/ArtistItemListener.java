4f437243558a96ee4fe7ac063acbcf037bc934d6
Ngocnkn <ngockhac01012000@gmail.com>
2019-12-14 15:26:34 +0700

    dự án 1

--- Du_an_1_App_Music/app/src/main/java/com/example/du_an_1_app_music/fragment/artist/ArtistItemListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.du_an_1_app_music.fragment.artist;
-
-import com.example.du_an_1_app_music.base.BaseAdapter;
-import com.example.du_an_1_app_music.model.Artist;
-
-public interface ArtistItemListener extends BaseAdapter.BaseItemListener {
-    void onArtistClick(Artist artist);
-}
--- Du_an_1_App_Music/app/src/main/java/com/example/du_an_1_app_music/fragment/song/SongItemListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.du_an_1_app_music.fragment.song;
-
-import com.example.du_an_1_app_music.base.BaseAdapter;
-import com.example.du_an_1_app_music.model.Song;
-
-public interface SongItemListener extends BaseAdapter.BaseItemListener {
-    void onSongClick(Song song);
-}
